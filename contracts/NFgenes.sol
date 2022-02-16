9a750b8e5e2afdd1e8e72e590bd6b155f3dbbde0
joselvelez <joselvelez@protonmail.com>
2022-02-15 23:40:08 -0300

    cleanup, work on tokenURI

--- contracts/NFgenes.sol
+++ contracts/NFgenes.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.11;
 
 /// @title NFgenes - Share knowledge, create value, build a community and teach science
-/// @notice Mint an NFT representing one of ~40k unique human genes.
+/// @notice Mint an NFT representing one of ~20k unique human genes.
 /// @author The team at the NFgenes Project https://github.com/orgs/nfgenes/people
 
 import "hardhat/console.sol";
@@ -19,8 +19,6 @@ contract NFgenes is
     Ownable,
     VPBM
 {
-    /// remove this strings declaration, was just using for testing purposes <======================================================
-    using Strings for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private tokenIdCounter;
 
@@ -30,8 +28,8 @@ contract NFgenes is
     /// @dev maintain a count for number of NFgenes currently minted
     uint public currentMintCount;
 
-    /// @notice mapping of gene symbols (stored as bytes32) to addresses
-    mapping(bytes32 => address) public owners;
+    /// @notice mapping of gene symbols (tokenId) to addresses
+    mapping(uint256 => address) public owners;
 
     // mapping(string => string) geneSymbolToName;
     // mapping(string => string) geneSymbolToLength;
@@ -43,17 +41,18 @@ contract NFgenes is
     constructor(string memory _initialBaseURI, bytes32 _rootHash)
         ERC721("NFgenes", "GENE")
         VPBM(_rootHash) {
+        
         // @dev set initial base URI
         baseURI = _initialBaseURI;
-    }
 
-    // function generateTokenMetadata() public onlyOwner returns (string memory) {
-    
-    // }
+        /// @dev set initial tokenId to 1 instead of 0
+        tokenIdCounter.increment();
+    }
 
     /// @dev All NFgenes data will be stored on IPFS, but the CID could possibly change,
     /// so while an initial baseURI will be set in the constructor, if a new list of genes
     /// is published to IPFS, then the 'setBaseURI' method will be called to update it
+    /// @notice Change the base URI, only the contract owner can do this
     /// @param _newBaseURI the new IPFS CID for the root folder containing all NFgenes
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
         baseURI = _newBaseURI;
@@ -68,27 +67,23 @@ contract NFgenes is
         super._burn(_tokenId);
     }
 
-    /// @dev override to allow changing the baseURI inherited from ERC721.sol
+    /// @dev override to display the current baseURI
     /// @return the string value of the baseURI (IPFS CID Hash)
     function _baseURI() internal view override(ERC721) returns (string memory) {
         return baseURI;
     } 
 
-    // @param _tokenId the gene symbol
-    function tokenURI(bytes32 _tokenId) public view override returns (bytes32) {
-        require(_exists(_tokenId), "Token does not exist");
-
-        string memory tokenId = string(abi.encodePacked(baseURI, _tokenId));
-
-        return tokenId;
+    /// @param tokenId the gene symbol
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        require(_exists(tokenId), "Token does not exist");
+        return super.tokenURI(tokenId);
     }
 
-    function _exists(bytes32 tokenId) internal view override returns (bool) {
-        return owners[tokenId] != address(0);
-    }
+    function mintGene() external {
+        /// @dev get the current tokenId and assign to the new mint
+        uint256 newTokenId = tokenIdCounter.current();
 
-    // function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
-    //     require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-    //     return tokenId.toString();
-    // }
+        /// @dev assign the new tokenId to the calling address
+        _safeMint(msg.sender, newTokenId);
+    } 
 }
